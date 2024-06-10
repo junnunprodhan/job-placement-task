2f7284b5d10980b985b344f5ff1b3a850371268d
junnun15 <junnunprodhan@gmail.com>
2024-06-10 19:14:46 +0600

    Problem: Solving the N-Queens Problem

--- index.js
+++ index.js
@@ -595,3 +595,49 @@ async function getData() {
 }
 
 getData();
+
+// 4. Recursion
+// Problem: Solving the N-Queens Problem
+function solveNQueens(n) {
+  const board = Array(n).fill().map(() => Array(n).fill('.'));
+  const res = [];
+
+  function isValid(board, row, col) {
+    for (let i = 0; i < row; i++) {
+      if (board[i][col] === 'Q') return false;
+    }
+    for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+      if (board[i][j] === 'Q') return false;
+    }
+    for (let i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+      if (board[i][j] === 'Q') return false;
+    }
+    return true;
+  }
+
+  function backtrack(row) {
+    if (row === n) {
+      res.push(board.map(row => row.join('')));
+      return;
+    }
+
+    for (let col = 0; col < n; col++) {
+      if (!isValid(board, row, col)) continue;
+      board[row][col] = 'Q';
+      backtrack(row + 1);
+      board[row][col] = '.';
+    }
+  }
+
+  backtrack(0);
+  return res;
+}
+
+console.log(solveNQueens(4));
+// Output: 
+// [
+//   '.Q..',
+//   '...Q',
+//   'Q...',
+//   '..Q.'
+// ]
