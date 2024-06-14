7cdf15cbdc7c364af77412b840fadee4095554dd
junnun15 <junnunprodhan@gmail.com>
2024-06-14 18:05:42 +0600

    Define multiple signatures for a function.

--- index.ts
+++ index.ts
@@ -8,3 +8,14 @@ function reverse<T>(items: T[]): T[] {
   console.log(reverse([1, 2, 3])); // Output: [3, 2, 1]
   console.log(reverse(['a', 'b', 'c'])); // Output: ['c', 'b', 'a']
   
+//   Function Overloading
+// Define multiple signatures for a function.
+
+function concat(a: string, b: string): string;
+function concat(a: number, b: number): number;
+function concat(a: any, b: any): any {
+  return a + b;
+}
+
+console.log(concat(1, 2)); // Output: 3
+console.log(concat("Hello, ", "World!")); // Output: Hello, World!
