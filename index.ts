cefb03a644fbff648d0ae8a3774a4f275ca01c1f
junnun15 <junnunprodhan@gmail.com>
2024-06-14 18:06:37 +0600

    Use decorators to modify behavior or add metadata.

--- index.ts
+++ index.ts
@@ -19,3 +19,33 @@ function concat(a: any, b: any): any {
 
 console.log(concat(1, 2)); // Output: 3
 console.log(concat("Hello, ", "World!")); // Output: Hello, World!
+
+// Decorator
+// Use decorators to modify behavior or add metadata.
+function Log(target: any, key: string) {
+    let value = target[key];
+  
+    const getter = () => value;
+    const setter = (newValue) => {
+      console.log(`Setting value for ${key}: ${newValue}`);
+      value = newValue;
+    };
+  
+    Object.defineProperty(target, key, {
+      get: getter,
+      set: setter,
+    });
+  }
+  
+  class Product {
+    @Log
+    price: number;
+  
+    constructor(price: number) {
+      this.price = price;
+    }
+  }
+  
+  const product = new Product(100);
+  product.price = 200; // Console: Setting value for price: 200
+  
