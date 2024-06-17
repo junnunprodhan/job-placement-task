9368be5bca2c39ed53015636ca7a47c6ab4c172b
junnun15 <junnunprodhan@gmail.com>
2024-06-17 22:52:58 +0600

    Remove Duplicates from an Array

--- index.js
+++ index.js
@@ -1000,4 +1000,5 @@ console.log(mergeArrays([1, 3, 5], [2, 4, 6])); // [1, 2, 3, 4, 5, 6]
 function removeDuplicates(arr) {
   return [...new Set(arr)];
 }
+
 console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // [1, 2, 3, 4, 5]
