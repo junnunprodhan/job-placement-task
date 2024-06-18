5a5d9a1471f2ab285e443970b841437cc89bc3ae
junnun15 <junnunprodhan@gmail.com>
2024-06-18 21:25:15 +0600

    Remove Duplicates from an Array

--- index.js
+++ index.js
@@ -930,6 +930,7 @@ function binarySearch(arr, target) {
   let right = arr.length - 1;
 
   while (left <= right) {
+    
     const mid = Math.floor((left + right) / 2);
     if (arr[mid] === target) return mid;
     if (arr[mid] < target) left = mid + 1;
